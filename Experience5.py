c89ec3b08d48176bcbd69acecb671450d405e4e9
Klynda <10055658+Klynda@users.noreply.github.com>
2017-09-01 07:36:36 +0200

    Add files via upload

---
+++ Experience5.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 18 16:01:07 2017
+
+@author: khiri
+"""
+
+from generateur import generateur3, generateur2
+from Traces import stockerTraces, lireTraces
+#from ModeleProba import Modele
+from ModeleEtat3 import Modele
+import cPickle as pickle
+from Traces import lireTraces
+import os
+
+#nombre de pas de temps
+nbStep = 200
+#nombre de zones
+N = 3
+#nombre de points de rencontre
+pointRencontre = 2
+#nombre d'individus
+M=1000
+
+tauxH_PR = 0.
+tauxDeplacement=0.5
+tauxSejour=0.4
+
+# LES DONNÉES ON DÉJÀ ÉTÉ GÉNÉRÉE ET STOCKÉE -> PLUS QU'À LIRE LE FICHIER TRACE
+
+#repartition, residence, paternsIndividus, listIndMobiles, paths, presences, endemicites = generateur3(N, M, pointRencontre, tauxH_PR, tauxDeplacement, tauxSejour, endemicites='0')
+
+path  = "./Experience5/"
+if not os.path.exists(path):
+    os.makedirs(path)
+        
+fic = path+"traces.txt"
+
+#stockerTraces(fic, N, M, pointRencontre, tauxH_PR, tauxDeplacement, tauxSejour, residence, paternsIndividus, listIndMobiles, paths, presences, endemicites)
+    
+N, M, pointRencontre, alpha, seuil, tauxSejour, residence, paternIndividus, listIndMobiles, paths, presences, endemicites = lireTraces(fic)
+
+#permet de générer les mêmes seuil d'exposition des individus d'un test à l'autre
+
+m = Modele(N, M)
+m.residences = residence
+m.initZones(endemicites)
+zones = m.getZones()
+humans = m.getHumans()
+humansList = humans.keys()
+
+
+#Tous les individus sont sains
+listI = []
+m.initStatesAndProba2(listI)
+
+#initialisation de la proportion de moustiques infectés à 80% dans les zones de rencontre
+zones['Z1'].Iv[0] = 30
+zones['Z2'].Iv[0] = 30
+#Rappel : le nombre de vecteur est initialisé à 100 par défaut
+
+'''
+# Démoustication à t=10 de Z1
+m.tDem = 10
+m.pDem = 0.95
+m.idDem = 'Z1'
+'''
+
+
+'''
+# Démoustication à t=10 de Z2'''
+
+
+m.tDem = 10
+m.pDem = 0.95
+m.idDem = 'Z2'
+
+
+while m.cTime <= nbStep :
+    t= m.cTime
+    print "T == "+str(t)
+    #initialisation des déplacements
+    m.initTravels(paths, presences)
+    m.updateModel()
+
+fic = path + "modele.p"
+#fic = path + "modele_dem1.p"
+#fic = path + "modele_dem2.p"
+fh = open (fic, 'wb')
+pickle.dump(m,fh,pickle.HIGHEST_PROTOCOL)
+fh.close()
+
+from PlotZone import *
+plot_AllPropIH(zones, path, tauxSejour)
+plot_AllPropEHr(zones, path, tauxSejour)
+plot_AllPropE0(zones, path, tauxSejour)
+plot_AllPropEH(zones, path, tauxSejour)
--- generateur.py
+++ generateur.py
@@ -54,9 +54,10 @@ def setDeplacements2Zones1PR(zones, humans, listeMobiles, tauxSejour, idHpr, idP
                 residences[idH]=idZ
     return paths, presences, residences
 
-#GENERATION DES LIEU DE RÉSIDENCES
+
 
 def generateur1(N, M, pointRencontre, alpha, seuil, Qs = None, endemicites = None):
+    #GENERATION DES LIEU DE RÉSIDENCES
     #initialisation nbre de zones
     n = N
     
@@ -249,6 +250,188 @@ def generateur2(residence) :
             presences[idZ] = [(idH, 1)]
     return path, presences
 
+
+def generateur3(N, M, pointRencontre, alpha, seuil, Qs = None, endemicites = None):
+    #GENERATION DES LIEU DE RÉSIDENCES
+    #initialisation nbre de zones
+    n = N
+    
+    #initialisation nbre d'humains
+    m= M
+    
+    #initialisation nbre de point de rencontre
+    pr = pointRencontre
+    
+    #pourcentation population pr
+    pppr = alpha
+    
+    #lieu de résidence de chaque individu
+    residence={}
+    
+    ###################################################################
+    #répartition du nombre d'individus sur les zones de regrouprement
+    repartition ={}
+    frequentation ={}
+    # nbre d'individu par zones de rencontre
+    if pr != 0 :
+        #population pr
+        popPr = int(pppr * m)
+        nbIndPr = popPr / pr
+        k = 1
+        for i in range(pr-1):
+            listInd = []
+            identifiant = 'Z'+str(i+1)
+            frequentation[identifiant] = input("Entrez frequentation "+identifiant+", SVP : ")
+            for j in range(nbIndPr) :
+                listInd.append('H'+str(k))
+                residence['H'+str(k)] = identifiant
+                k +=1
+            repartition[identifiant] = listInd
+        if pr != 0:
+            identifiant = 'Z'+str(pr)
+            frequentation[identifiant] = input("Entrez frequentation "+identifiant+", SVP : ")
+            listInd = []
+            for j in range(nbIndPr + popPr % pr) :
+                listInd.append('H'+str(k))
+                residence['H'+str(k)] = identifiant
+                k +=1
+        repartition[identifiant] = listInd
+    else :
+        popPr = 0
+        k = 1
+    #print "popPr = " +str(popPr)   
+    
+    ############################################################
+    #répartition sur les zones restantes
+    
+    #nb de zones restantes
+    zr = n - pr
+    
+    
+    #nb d'individus dans le reste des zones
+    popZ = m - popPr
+    
+    
+    #nbre d'individus par zones restantes
+    if zr != 0 :
+        nbIndZ = popZ / zr
+    
+
+    
+        for i in range(pr, n, 1):
+            listInd = []
+            identifiant = 'Z'+str(i+1)
+            for j in range(nbIndZ):
+                listInd.append('H'+str(k))
+                residence['H'+str(k)] = identifiant
+                k +=1
+            repartition[identifiant] = listInd
+        
+        listInd = []
+        identifiant = 'Z'+str(n)
+        for j in range(popZ % zr) :
+            listInd.append('H'+str(k))
+            residence['H'+str(k)]=identifiant
+            k +=1
+        repartition['Z'+str(n)] = listInd
+    
+    ########################################################
+    #Calcul du patern pour chaque individu
+    path = {}
+    presences = {}
+    paternsIndividus = {}
+    listIndMobiles = []
+    
+    for idH, idZ in residence.iteritems():
+        pd = random.random()
+        if Qs is None :
+            qs = random.random()
+        else :
+            qs = Qs
+        prChoisis = []
+        path[idH] = []
+        
+        #----------------------
+        lpr = range(1, pr+1, 1)
+        listPr = []
+        for li in lpr :
+            listPr.append('Z'+str(li))
+        #----------------------
+       
+            
+        if not(listPr.__contains__(idZ)) :
+            if pd<= seuil and pr != 0 :
+                 #ajout du ratio temps dans la zone de résidence
+                if presences.__contains__(idZ) :
+                    presences[idZ].append( (idH, 1 - qs) )
+                else :
+                    presences[idZ] = [(idH, 1 - qs)]
+                path[idH].append( (idZ, 1 - qs) )
+                
+                #jouter l'individu dans la liste des mobiles
+                listIndMobiles.append(idH)
+                
+                #si la valeur est supérieure au seuil choisir uniformement un 
+                #nombre de point de rencontre parmi ceux connus
+                nbpr = random.randint(1, pr)
+                
+                #liste des points de rencontre disponibles
+                listPr = range(1, pr+1, 1)
+                # ICI PASSE PAR TOUS LES POINTS DE RENCONTRE
+                for i in range (pr):
+                    zChoisie = 'Z'+str(listPr[i])
+                    prChoisis.append(zChoisie)
+                    path[idH].append( (zChoisie, qs*frequentation[zChoisie]) )
+                    if presences.__contains__(zChoisie) :
+                        presences[zChoisie].append((idH, qs*frequentation[zChoisie]))
+                    else :
+                        presences[zChoisie] = [(idH, qs*frequentation[zChoisie])]
+            else :
+                path[idH]= [(idZ, 1)]
+                if presences.__contains__(idZ) :
+                    presences[idZ].append( (idH, 1) )
+                else :
+                    presences[idZ] = [(idH, 1)]
+        else :
+            prChoisis = []
+            path[idH]= [(idZ, 1)]
+            if presences.__contains__(idZ) :
+                presences[idZ].append( (idH, 1) )
+            else :
+                presences[idZ] = [(idH, 1)]
+        paternsIndividus[idH] = (idZ, pd, qs, prChoisis)
+        
+    ######################################################
+    
+    #liste des endémicités : est représentée par un dictionnaire {idZ : K}
+    endemicite = {}
+    
+    #valeurs extrèmes pour les points d'interêts (K=0 ou K=1) on alterne entre les zones
+    for i in range(pr):
+        identifiant = 'Z'+str(i+1)
+        if endemicites is None :
+            endemicite[identifiant] = 1 - i%2
+        else :
+            endem = input("Entrez l'endémicité de la zone de rencontre "+identifiant+", SVP : ")
+            while (endem < 0 or endem >1) :
+                endem = input("La valeur doit être comprise entre 0 et 1 : ")
+            endemicite[identifiant] = endem
+        
+    #pour le reste des zones valeur aléatoire entre 0 et 1
+    for i in range(pr, n, 1):
+        identifiant = 'Z'+str(i+1)
+        if endemicites is None :
+            endemicite[identifiant] = random.random()
+        else :
+            endem = input("Entrez l'endémicité de la zone de résidence "+identifiant+", SVP : ")
+            while (endem < 0 or endem >1) :
+                endem = input("La valeur doit être comprise entre 0 et 1 : ")
+            endemicite[identifiant] = endem
+    
+    
+    
+    
+    return repartition, residence, paternsIndividus, listIndMobiles, path, presences, endemicite
 '''
 N = 3
 M = 9
